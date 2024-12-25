f1c92ee3827e286c0126eeef892d7ea997321476
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2024-12-25 11:35:40 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Toaster } from "@/components/ui/toaster";
+import logo from "@/public/logo.png"
 
 export const metadata: Metadata = {
   title: "Zikoro - Credentials",
@@ -12,13 +13,13 @@ export const metadata: Metadata = {
 
   openGraph: {
     type: "website",
-    url: "https://credentials.zikoro.com/logo.png",
+    url: '/logo.png',
     title: "Zikoro - Credentials",
     description:
       "Secure and Verifiable Digital Credentials for Achievements. Create, manage and share digital certificates and badges in 5 minutes",
     images: [
       {
-        url: "https://credentials.zikoro.com/logo.png",
+        url: '/zikoro-og.jpeg',
         width: 115,
         height: 40,
         alt: "Zikoro - Credentials",
