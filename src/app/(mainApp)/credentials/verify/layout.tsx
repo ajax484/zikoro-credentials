f7d30458dec7e1ad4159f6912ff69b11f70ec176
Bilal Yusuf <ubahyusuf484@gmail.com>
2025-01-04 21:56:39 +0100

    push

--- src/app/(mainApp)/credentials/verify/layout.tsx
+++ src/app/(mainApp)/credentials/verify/layout.tsx
@@ -2,18 +2,14 @@
 import React from "react";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
+import logo from "@/public/icons/logo.svg";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <header className="py-2.5 px-4 bg-white fixed w-screen z-[100]">
         <a href="https://zikoro.com" rel="noopener noreferrer">
-          <Image
-            src={"/logo.png"}
-            alt={"zikoro logo"}
-            width={100}
-            height={50}
-          ></Image>
+          <Image src={logo} alt={"zikoro logo"} width={100} height={50}></Image>
         </a>
       </header>
       <main className="bg-baseBody">{children}</main>
