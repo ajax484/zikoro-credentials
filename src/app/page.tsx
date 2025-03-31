b2e15e9065f2bdd357da1cdf44e70dec36ac5440
EmmanuelN1 <emmanuelnzeakor00@yahoo.com>
2025-03-31 11:04:43 +0100

    fix: added breadcumbs to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,67 @@ import Section2 from "@/components/home/Section2";
 import Section3 from "@/components/home/Section3";
 import Section4 from "@/components/home/Section4";
 import Section5 from "@/components/home/Section5";
+import Head from "next/head";
 
 export default function Home() {
+  const breadcrumbStructuredData = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Events",
+        item: "https://www.zikoro.com/",
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Bookings",
+        item: "https://bookings.zikoro.com/",
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: "Engagements",
+        item: "https://engagements.zikoro.com/",
+      },
+      {
+        "@type": "ListItem",
+        position: 4,
+        name: "Concerts",
+        item: "https://www.zikoro.com/concerts",
+      },
+      {
+        "@type": "ListItem",
+        position: 5,
+        name: "Pricing",
+        item: "https://www.zikoro.com/pricing",
+      },
+    ],
+  };
   return (
-    <div className="bg-[#f7f7f7]">
-      <div className="sticky top-4 z-50">
-        <Navbar />
+    <>
+      {" "}
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(breadcrumbStructuredData),
+          }}
+        />
+      </Head>
+      <div className="bg-[#f7f7f7]">
+        <div className="sticky top-4 z-50">
+          <Navbar />
+        </div>
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Footer />
       </div>
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
-      <Footer />
-    </div>
+    </>
   );
 }
